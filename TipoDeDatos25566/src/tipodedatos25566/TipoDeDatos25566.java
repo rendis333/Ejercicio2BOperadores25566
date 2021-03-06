9cf6c1676eb00556efb4e456b120b2ad6f351516
escuela <escuela@EC26>
2021-03-05 19:31:34 -0600

    tipo de datos

---
+++ TipoDeDatos25566/src/tipodedatos25566/TipoDeDatos25566.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos25566;
+
+/**
+ *
+ * @author escuela
+ */
+public class TipoDeDatos25566 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Valor Máximo Byte: " + Byte.MAX_VALUE);
+System.out.println("Valor Mínimo Byte: " + Byte.MIN_VALUE);
+
+System.out.println("Tipo\tMínimo\tMáximo");
+System.out.println("byte\t" + Byte.MIN_VALUE + "\t" + Byte.MAX_VALUE);
+System.out.println("short\t" + Short.MIN_VALUE + "\t" + Short.MAX_VALUE);
+System.out.println("int\t" + Integer.MIN_VALUE + "\t" + Integer.MAX_VALUE);
+System.out.println("long\t" + Long.MIN_VALUE + "\t" + Long.MAX_VALUE);
+System.out.println("float\t" + Float.MIN_VALUE + "\t" + Float.MAX_VALUE);
+System.out.println("double\t" + Double.MIN_VALUE + "\t" + Double.MAX_VALUE);
+    }
+    
+}
